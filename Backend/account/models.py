a6720e1c9c1832be904d730e81062d939c87b287
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-02-10 05:29:51 +0100

    Refactor user model and manager: implement user creation logic and define roles

--- Backend/account/models.py
+++ Backend/account/models.py
@@ -1,3 +1,67 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
+# from .managers import UserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            full_name=full_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, full_name, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        STUDENT = "student", "Student"
+        LECTURER = "lecturer", "Lecturer"
+        ADMIN = "admin", "Admin"
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    class Meta:
+        ordering = ["-date_joined"]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f"{self.full_name} ({self.role})"
