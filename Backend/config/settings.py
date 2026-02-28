6c3c173f2bcd0f93c21bbbd896237a93d56819ef
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-02-28 20:46:53 +0100

    updated settings.py

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -24,7 +24,7 @@ import os
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
