97eea9cd8156fa9dbcc2e4f490dbbb8afbdf0f0a
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-03-03 12:17:04 +0100

    feat: Add bio field to User model and implement profile update serializers for Student and Lecturer

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.http import HttpResponse
 from django.urls import path
-from .views import MeView, SignupView, LoginView
+from .views import MeView, SignupView, LoginView, StudentProfileUpdateView, LecturerProfileUpdateView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("me/", MeView.as_view(), name="me"),
+    path("student/profile/update/", StudentProfileUpdateView.as_view(), name="student-profile-update"),
+    path("lecturer/profile/update/", LecturerProfileUpdateView.as_view(), name="lecturer-profile-update",
+    ),
 ]
